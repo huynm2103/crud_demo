13dcfeef2e38e8a4ab31590c93209cccf9735d2d
huynm_int <huynm_int@newwave.vn>
2014-12-16 09:54:43 +0700

    Update delete form

--- app/views/students/index.blade.php
+++ app/views/students/index.blade.php
@@ -21,7 +21,7 @@
 
     <h1>All the students</h1>
 
-    <!-- will be used to show any messages -->
+    <!-- show message -->
     @if (Session::has('message'))
     <div class="alert alert-info">{{ Session::get('message') }}</div>
     @endif
@@ -50,20 +50,19 @@
                 <td>female</td>
             @endif
 
-            <!-- we will also add show, edit, and delete buttons -->
+            <!-- show, edit, and delete buttons -->
             <td>
 
-                <!-- delete the nerd (uses the destroy method DESTROY /nerds/{id} -->
-                <!-- we will add this later since its a little more complicated than the other two buttons -->
-                {{ Form::open(array('url' => 'students/' . $value->id, 'class' => 'pull-right')) }}
-                {{ Form::hidden('_method', 'DELETE') }}
-                {{ Form::submit('Delete this Nerd', array('class' => 'btn btn-warning')) }}
-                {{ Form::close() }}
+                <!-- delete button -->
+                <form class="pull-right" method="POST" action="{{ URL::to('students/'. $value->id) }}">
+                    <input name="_method" type="hidden" value="DELETE" />
+                    <input class="btn btn-warning" type="submit" value="Delete this Student" />
+                </form>
 
-                <!-- show the nerd (uses the show method found at GET /nerds/{id} -->
+                <!-- Show detail button -->
                 <a class="btn btn-small btn-success" href="{{ URL::to('students/' . $value->id) }}">Show this Nerd</a>
 
-                <!-- edit this nerd (uses the edit method found at GET /nerds/{id}/edit -->
+                <!-- edit button -->
                 <a class="btn btn-small btn-info" href="{{ URL::to('students/' . $value->id . '/edit') }}">Edit this Nerd</a>
 
             </td>
